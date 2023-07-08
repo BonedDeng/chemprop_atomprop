f6c0c19da03f055a2164d1ce89197fb35a0eecb8
BonedDeng <105792997+BonedDeng@users.noreply.github.com>
2023-07-08 23:24:00 +0800

    Update model.py

--- chemprop/models/model.py
+++ chemprop/models/model.py
@@ -12,19 +12,19 @@ from chemprop.features import BatchMolGraph
 from chemprop.nn_utils import initialize_weights
 
 
-class MLP(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(MLP, self).__init__()
+# class MLP(nn.Module):
+#     def __init__(self, input_size, hidden_size, output_size):
+#         super(MLP, self).__init__()
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.relu = nn.ReLU()
+#         self.fc2 = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+#     def forward(self, x):
+#         out = self.fc1(x)
+#         out = self.relu(out)
+#         out = self.fc2(out)
+#         return out
 
 
 class MoleculeModel(nn.Module):
